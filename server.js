e8bf23d658a5e841a53d89a94aa6091055beef24
vanshika-05 <66822404+vanshika-05@users.noreply.github.com>
2023-08-17 11:55:35 +0100

    script update - vanshika

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = require("./app/models");
 
 db.sequelize.sync();
-// // drop the table if it already exists
+// drop the table if it already exists
   // db.sequelize.sync({ force: true }).then(() => {
   //    console.log("Drop and re-sync db.");
   //  });
